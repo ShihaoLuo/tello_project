133d79dc732577ed0abe2296dfe38ab68d671585
ShihaoLuo <a5510925475>
2020-10-01 20:28:06 +0800

    '202010012028'

--- classifier/trainer.py
+++ classifier/trainer.py
@@ -8,7 +8,7 @@ Created on Wed Sep 30 16:11:59 2020
 
 import os
 import cv2
-
+import time
 
 def process_negpic(_folder):
     file_list = os.listdir(_folder)
@@ -31,12 +31,11 @@ def select_roi(img):
     cv2.putText(img, text, (img.shape[0]/15, img.shape[1]/15),
                 cv2.FONT_HERSHEY_SIMPLEX, 1, (55, 255, 155), 2)
     roi = cv2.selectROI('roi', img)
-    cv2.waitKey(0)
-    cv2.destroyWindow('roi')
     x, y, w, h = roi
-    dst = img.copy()
-    dst = dst[y:y+h, x:x+w]
-    cv2.imwrite('/positive/positive0.jpg', dst)
+    dst = img.copy()[y:y+h, x:x+w]
+    cv2.imwrite('./positive/positive0.jpg', dst)
+    cv2.waitKey(5000)
+    cv2.destroyWindow('roi')
     return roi
 
 
@@ -59,9 +58,15 @@ def select_roi(img):
 
 
 def create_possample():
-    os.system('opencv_createsamples -img positive/positive1.jpg -num 100 -w 640 -h 480 -vec positives.vec -show -bg bg.txt')
+    os.system('opencv_createsamples -show -img positive/positive0.jpg -num 1500 -vec positives.vec -bg bg.txt -pngoutput info')
 
+def train():
+    if not os.path.exists('data'):
+        os.system('mkdir data')
+    os.system('opencv_traincascade -data data -vec positives.vec -bg bg.txt -numPos 1000 -numNeg 500  -numStages 15')
 
-# process_negpic('negative/')
-img = cv2.imread('../env_pics/0_3_270_1.jpg', 0)
-select_roi(img)
+#process_negpic('negative/')
+#img = cv2.imread('../env_pics/0_3_270_1.jpg', 0)
+#select_roi(img)
+#create_possample()
+train()
